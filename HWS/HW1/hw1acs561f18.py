1466354669348fb8c414ed303793cfc1c59b4560
Yutao_Ren <yutaoren118@gmail.com>
2018-09-07 13:43:55 -0700

    format adjustment

--- HWS/HW1/hw1acs561f18.py
+++ HWS/HW1/hw1acs561f18.py
@@ -4,8 +4,8 @@ with open("input.txt") as input_file:
         for line in lines:
             data = line.split(",")
             if data[1] == "Dirty":
-                output_file.write("Suck\n");
+                output_file.write("Suck\n")
             elif data[0] == "A":
-                output_file.write("Right\n");
+                output_file.write("Right\n")
             elif data[0] == "B":
-                output_file.write("Left\n");
+                output_file.write("Left\n")
