14929d183195799775bd94a2f39654cc287b7caf
Yutao_Ren <yutaoren118@gmail.com>
2018-09-07 13:27:00 -0700

    create HW1

---
+++ HWS/HW1/hw1acs561f18.py
@@ -0,0 +1,11 @@
+with open("input.txt") as input_file:
+    lines = input_file.read().splitlines()
+    with open("output.txt", "w") as output_file:
+        for line in lines:
+            data = line.split(",")
+            if data[1] == "Dirty":
+                output_file.write("Suck\n");
+            elif data[0] == "A":
+                output_file.write("Right\n");
+            elif data[0] == "B":
+                output_file.write("Left\n");
