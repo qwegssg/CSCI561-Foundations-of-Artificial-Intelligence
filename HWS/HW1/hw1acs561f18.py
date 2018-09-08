0ec76fdb991a232f3a7eaa61e04b721f40bfb990
Yutao_Ren <yutaoren118@gmail.com>
2018-09-07 17:10:50 -0700

    Remove the new line at the end

--- HWS/HW1/hw1acs561f18.py
+++ HWS/HW1/hw1acs561f18.py
@@ -1,11 +1,15 @@
 with open("input.txt") as input_file:
     lines = input_file.read().splitlines()
     with open("output.txt", "w") as output_file:
+        counter = 0
         for line in lines:
             data = line.split(",")
             if data[1] == "Dirty":
-                output_file.write("Suck\n")
+                output_file.write("Suck")
             elif data[0] == "A":
-                output_file.write("Right\n")
+                output_file.write("Right")
             elif data[0] == "B":
-                output_file.write("Left\n")
+                output_file.write("Left")
+            if counter != (len(lines))-1:
+                output_file.write("\n")
+                counter++
