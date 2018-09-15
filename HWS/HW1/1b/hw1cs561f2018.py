9959f6a3fa172bbda3e1fa27f5c8144af810f81e
Yutao_Ren <yutaoren118@gmail.com>
2018-09-15 00:37:14 -0700

    achieve H1b

--- HWS/HW1/1b/hw1cs561f2018.py
+++ HWS/HW1/1b/hw1cs561f2018.py
@@ -1,43 +1,62 @@
 class SolveScooters:
 
-    def create_areas(self, n):
+    # n: the length of the area
+    # p: the number of police officers
+    def create_areas(self, n, p):
         # result is a list of lists, if existing
         result = []
+        # max = 0
         visited_area = {
             'col': set(),
             'sum': set(),
             'diff': set(),
         }
-        self.dfs(n, [], visited_area, result)
+        self.dfs(n, p, [], visited_area, 0, 0, result)
         return result
         # return len(result)
 
     # the fun part
-    def dfs(self, n, permutation, visited_area, result):
-        if len(permutation) == n:
-            result.append(self.construct_graph(permutation))
+    # count: the number of police officers placed
+    # interval: the number of rows left empty
+    def dfs(self, n, p, graph_list, visited_area, count, interval, result):
+        # if len(graph_list) == n:
+        if count == p:
+            result.append(self.construct_graph(graph_list))
             return
 
-        row = len(permutation)
+        row = len(graph_list)
         # check every column in one level (one row)
         for col in range(n):
-            if not self.check_valid(permutation, visited_area, col):
+            if not self.check_valid(graph_list, visited_area, col):
                 continue
-            permutation.append(col)
+            # if the new node is valid, then add it to graph list
+            # graph_list.append(col)
+            graph_list.append(str(row) + "," + str(col))
+            count += 1
             visited_area['col'].add(col)
             visited_area['sum'].add(row + col)
             visited_area['diff'].add(row - col)
 
-            self.dfs(n, permutation, visited_area, result)
+            self.dfs(n, p, graph_list, visited_area, count, interval, result)
+
             # backtracking! return to the father node above to try other child nodes
             visited_area['col'].remove(col)
             visited_area['sum'].remove(row + col)
             visited_area['diff'].remove(row - col)
-            permutation.pop()
+            graph_list.pop()
+            count -= 1
+
+        if interval < n - p:
+            graph_list.append(-1)
+            interval += 1
+            self.dfs(n, p, graph_list, visited_area, count, interval, result)
+            # backtracking!
+            graph_list.pop()
+            interval -= 1
 
     # check if the new police officer's position is conflicted with old ones
-    def check_valid(self, permutation, visited_area, col):
-        row = len(permutation)
+    def check_valid(self, graph_list, visited_area, col):
+        row = len(graph_list)
         if col in visited_area['col']:
             return False
         if row + col in visited_area['sum']:
@@ -47,14 +66,40 @@ class SolveScooters:
         return True
 
     # every graph is a list
-    def construct_graph(self, permutation):
-        graph = []
-        n = len(permutation)
-        for col in permutation:
-            row_string = ''.join(['p' if c == col else '.' for c in range(n)])
-            graph.append(row_string)
+    def construct_graph(self, graph_list):
+        # graph = []
+        # n = len(graph_list)
+        # for col in graph_list:
+        #     row_string = ''.join(['p' if c == col else '.' for c in range(n)])
+        #     graph.append(row_string)
+        # return graph
+        graph = set()
+        for p in graph_list:
+            graph.add(p)
         return graph
 
 
+
+    def get_max(self):
+        max_point = 0
+        with open("input1.txt") as input_file:
+            lines = input_file.read().splitlines()
+            result = self.create_areas(int(lines[0]), int(lines[1]))
+            print(result)
+            for graph in result:
+                point = 0
+                for line in lines:
+                    # data = line.split(",")
+                    # print(line)
+                    if line in graph:
+                        point += 1
+                if point > max_point:
+                    max_point = point
+        with open("output.txt", "w") as output_file:
+            output_file.write(str(max_point) + "\n")
+
+
 solution = SolveScooters()
-print(solution.create_areas(4))
+# print(solution.create_areas(4, 2))
+# result = solution.create_areas(4, 2)
+solution.get_max()
