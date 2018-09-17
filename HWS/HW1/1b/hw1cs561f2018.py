89a2a807e9f6b6e557729501ac1b294cf00982e5
Yutao_Ren <yutaoren118@gmail.com>
2018-09-16 23:58:12 -0700

    edit variable name, add new test input file

--- HWS/HW1/1b/hw1cs561f2018.py
+++ HWS/HW1/1b/hw1cs561f2018.py
@@ -5,76 +5,70 @@ class SolveScooters:
     def create_areas(self, n, p):
         # result is a list of lists, if existing
         result = []
-        # max = 0
-        visited_area = {
+        occupied = {
             'col': set(),
             'sum': set(),
             'diff': set(),
         }
-        self.dfs(n, p, [], visited_area, 0, 0, result)
+        self.dfs(n, p, [], occupied, 0, 0, result)
         return result
-        # return len(result)
 
-    # the fun part
+    # the fun part.
     # count: the number of police officers placed
     # interval: the number of rows left empty
-    def dfs(self, n, p, graph_list, visited_area, count, interval, result):
-        # if len(graph_list) == n:
+    def dfs(self, n, p, officers, occupied, count, interval, result):
+        # if len(officers) == n:
         if count == p:
-            result.append(self.construct_graph(graph_list))
+            result.append(self.construct_graph(officers))
             return
-
-        row = len(graph_list)
         # check every column in one level (one row)
+        row = len(officers)
         for col in range(n):
-            if not self.check_valid(graph_list, visited_area, col):
+            if not self.check_valid(officers, occupied, col):
                 continue
-            # if the new node is valid, then add it to graph list
-            # graph_list.append(col)
-            graph_list.append(str(row) + "," + str(col))
+            # if the new node is valid, then add it to the list
+            # officers.append(col)
+            officers.append(str(row) + "," + str(col))
             count += 1
-            visited_area['col'].add(col)
-            visited_area['sum'].add(row + col)
-            visited_area['diff'].add(row - col)
+            occupied['col'].add(col)
+            occupied['sum'].add(row + col)
+            occupied['diff'].add(row - col)
 
-            self.dfs(n, p, graph_list, visited_area, count, interval, result)
+            self.dfs(n, p, officers, occupied, count, interval, result)
 
             # backtracking! return to the father node above to try other child nodes
-            visited_area['col'].remove(col)
-            visited_area['sum'].remove(row + col)
-            visited_area['diff'].remove(row - col)
-            graph_list.pop()
+            occupied['col'].remove(col)
+            occupied['sum'].remove(row + col)
+            occupied['diff'].remove(row - col)
+            officers.pop()
             count -= 1
 
         if interval < n - p:
-            graph_list.append(-1)
+            officers.append(-1)
             interval += 1
-            self.dfs(n, p, graph_list, visited_area, count, interval, result)
+            self.dfs(n, p, officers, occupied, count, interval, result)
             # backtracking!
-            graph_list.pop()
+            officers.pop()
             interval -= 1
 
     # check if the new police officer's position is conflicted with old ones
-    def check_valid(self, graph_list, visited_area, col):
-        row = len(graph_list)
-        if col in visited_area['col']:
+    def check_valid(self, officers, occupied, col):
+        row = len(officers)
+        if col in occupied['col'] or row + col in occupied['sum'] or row - col in occupied['diff']:
             return False
-        if row + col in visited_area['sum']:
-            return False
-        if row - col in visited_area['diff']:
-            return False
-        return True
+        else:
+            return True
 
     # every graph is a list
-    def construct_graph(self, graph_list):
+    def construct_graph(self, officers):
         # graph = []
-        # n = len(graph_list)
-        # for col in graph_list:
+        # n = len(officers)
+        # for col in officers:
         #     row_string = ''.join(['p' if c == col else '.' for c in range(n)])
         #     graph.append(row_string)
         # return graph
         graph = set()
-        for p in graph_list:
+        for p in officers:
             graph.add(p)
         return graph
 
@@ -82,10 +76,10 @@ class SolveScooters:
 
     def get_max(self):
         max_point = 0
-        with open("input1.txt") as input_file:
+        with open("input3.txt") as input_file:
             lines = input_file.read().splitlines()
             result = self.create_areas(int(lines[0]), int(lines[1]))
-            print(result)
+            # print(result)
             for graph in result:
                 point = 0
                 for line in lines:
@@ -95,6 +89,7 @@ class SolveScooters:
                         point += 1
                 if point > max_point:
                     max_point = point
+        print(max_point)
         with open("output.txt", "w") as output_file:
             output_file.write(str(max_point) + "\n")
 
